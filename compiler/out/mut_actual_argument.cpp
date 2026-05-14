01eed68b66b8363c3c7e213548669aea42463a2e
Arshansky Evgeny <arshansky@nordpointlp.com>
2026-05-14 18:56:27 +0400

    Rewrite mut_actual_argument codegen to use map and fold. Replace string concatenation with interpolation for prelude lines.

--- compiler/out/mut_actual_argument.cpp
+++ compiler/out/mut_actual_argument.cpp
@@ -20,41 +20,14 @@ bool sexpr_is_stable_reference_for_cpp_mut_parameter(std::shared_ptr<semantic_ir
 
 mlc::String wrap_invoke_with_mut_actual_argument_holder_prelude_if_any(mlc::String prelude_block, mlc::String invoke_expression) noexcept{return prelude_block.length() == 0 ? invoke_expression : mlc::String("[&]() {\n") + prelude_block + mlc::String("\nreturn ") + invoke_expression + mlc::String(";\n}()");}
 
-mut_actual_argument::MutActualArgumentMaterializationOutcome materialize_positional_actual_arguments_maybe_holding_mut_references(mlc::Array<std::shared_ptr<semantic_ir::SExpr>> positional_argument_expressions, mlc::Array<int> mut_parameter_flags, context::CodegenContext context, std::function<mlc::String(mlc::Array<std::shared_ptr<semantic_ir::SStmt>>, context::CodegenContext)> gen_stmts, std::function<mlc::String(std::shared_ptr<semantic_ir::SExpr>, context::CodegenContext, std::function<mlc::String(mlc::Array<std::shared_ptr<semantic_ir::SStmt>>, context::CodegenContext)>)> evaluate_expression) noexcept{
-int argument_count = positional_argument_expressions.size();
-mlc::String prelude_block = mlc::String("");
-mlc::Array<mlc::String> materialized_argument_fragments = {};
-return mut_parameter_flags.size() != argument_count ? [&]() -> mut_actual_argument::MutActualArgumentMaterializationOutcome { 
-  int plain_index = 0;
-  while (plain_index < argument_count){
-{
-materialized_argument_fragments.push_back(evaluate_expression(positional_argument_expressions[plain_index], context, gen_stmts));
-plain_index = plain_index + 1;
-}
-}
-  return mut_actual_argument::MutActualArgumentMaterializationOutcome{prelude_block, materialized_argument_fragments};
- }() : [&]() -> mut_actual_argument::MutActualArgumentMaterializationOutcome { 
-  int position_index = 0;
-  while (position_index < argument_count){
-{
-mlc::String evaluated_fragment = evaluate_expression(positional_argument_expressions[position_index], context, gen_stmts);
-if (mut_parameter_flags[position_index] != 0 && !sexpr_is_stable_reference_for_cpp_mut_parameter(positional_argument_expressions[position_index])){
-{
-mlc::String holder_variable_name = mlc::String("__mut_actual_argument_holder_") + mlc::to_string(position_index);
-mlc::String holder_type_spelling = type_gen::sem_type_to_cpp(context, semantic_ir::sexpr_type(positional_argument_expressions[position_index]));
-prelude_block = prelude_block + holder_type_spelling + mlc::String(" ") + holder_variable_name + mlc::String(" = ") + evaluated_fragment + mlc::String(";\n");
-materialized_argument_fragments.push_back(holder_variable_name);
-}
-} else {
-{
-materialized_argument_fragments.push_back(evaluated_fragment);
-}
-}
-position_index = position_index + 1;
-}
-}
-  return mut_actual_argument::MutActualArgumentMaterializationOutcome{prelude_block, materialized_argument_fragments};
- }();
-}
+mut_actual_argument::MutActualArgumentMaterializationOutcome materialize_positional_actual_arguments_maybe_holding_mut_references(mlc::Array<std::shared_ptr<semantic_ir::SExpr>> positional_argument_expressions, mlc::Array<int> mut_parameter_flags, context::CodegenContext context, std::function<mlc::String(mlc::Array<std::shared_ptr<semantic_ir::SStmt>>, context::CodegenContext)> gen_stmts, std::function<mlc::String(std::shared_ptr<semantic_ir::SExpr>, context::CodegenContext, std::function<mlc::String(mlc::Array<std::shared_ptr<semantic_ir::SStmt>>, context::CodegenContext)>)> evaluate_expression) noexcept{return mut_parameter_flags.size() != positional_argument_expressions.size() ? mut_actual_argument::MutActualArgumentMaterializationOutcome{mlc::String(""), positional_argument_expressions.map([evaluate_expression, context, gen_stmts](std::shared_ptr<semantic_ir::SExpr> expression) mutable { return evaluate_expression(expression, context, gen_stmts); })} : positional_argument_expressions.fold(mut_actual_argument::MutActualArgumentMaterializationOutcome{mlc::String(""), {}}, [evaluate_expression, context, gen_stmts, mut_parameter_flags](mut_actual_argument::MutActualArgumentMaterializationOutcome accumulator, std::shared_ptr<semantic_ir::SExpr> positional_argument_expression) mutable { return [&]() -> mut_actual_argument::MutActualArgumentMaterializationOutcome { 
+  int position_index = accumulator.materialized_argument_fragments.size();
+  mlc::String evaluated_fragment = evaluate_expression(positional_argument_expression, context, gen_stmts);
+  return mut_parameter_flags[position_index] != 0 && !sexpr_is_stable_reference_for_cpp_mut_parameter(positional_argument_expression) ? [&]() -> mut_actual_argument::MutActualArgumentMaterializationOutcome { 
+  mlc::String holder_variable_name = mlc::String("__mut_actual_argument_holder_") + mlc::to_string(position_index);
+  mlc::String holder_type_spelling = type_gen::sem_type_to_cpp(context, semantic_ir::sexpr_type(positional_argument_expression));
+  return mut_actual_argument::MutActualArgumentMaterializationOutcome{accumulator.prelude_block + holder_type_spelling + mlc::String(" ") + holder_variable_name + mlc::String(" = ") + evaluated_fragment + mlc::String(";\n"), accumulator.materialized_argument_fragments.concat(mlc::Array<mlc::String>{holder_variable_name})};
+ }() : mut_actual_argument::MutActualArgumentMaterializationOutcome{accumulator.prelude_block, accumulator.materialized_argument_fragments.concat(mlc::Array<mlc::String>{evaluated_fragment})};
+ }(); });}
 
 } // namespace mut_actual_argument
